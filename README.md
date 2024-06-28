5e6c1077c298e6d9950d1ae1e24335ef712a9c15
Marco Michelino <marco.michelino@immobiliare.it>
2024-06-28 17:44:14 +0200

    documentation

--- README.md
+++ README.md
@@ -2,6 +2,102 @@
 
 ## This blueprint has the following characteristics
 * Users have to register anonymously with their Radix wallet to get an NFT. A UserEvent is emitted when this happens.
+* User badges are soulbound.
 * The owner can deposit the rewards ahead of time (rug proof), before actually assigning them to users. Different rewards are allowed.
 * The presence of a scheduled offchain operation that takes a snapshot of the LSU distribution is required; this scheduled operation must inform the component about the rewards to be distributed.
 * Each user can withdraw his rewards whenever he wants.
+
+## Known limitations
+* Do not use more than 100 different rewards for stakers or the withdraw transactions may fail.
+* Do not assign a reward to more than 100 users in a single transaction or it may fail (issue multiple transactions each one containing a single assign\_rewards method call).
+
+## Below are the transaction manifests needed to use this contract:
+
+### Instantiate the component (Stokenet)
+```
+CALL_FUNCTION
+    Address("package_tdx_2_1p583d3dwvaq93r7t82fz9u4yls3ca7xgk8ec8wskkxw4q30n4jph3q")
+    "AddixFomoRewards"
+    "new"
+    Address("<OWNER_BADGE>")
+    Address("<AIRDROPPER_BADGE>")
+;
+```
+
+### Mint an user badge
+```
+CALL_METHOD
+    Address("<COMPONENT_ADDRESS>")
+    "mint_user_nft"
+;
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "deposit_batch"
+    Expression("ENTIRE_WORKTOP")
+;
+```
+
+### Deposit rewards (owner only)
+```
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "create_proof_of_amount"
+    Address("<OWNER_BADGE>")
+    Decimal("1")
+;
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "withdraw"
+    Address("<REWARDS_RESOURCE_ADDRESS>")
+    Decimal("<AMOUNT_TO_DEPOSIT>")
+;
+TAKE_ALL_FROM_WORKTOP
+    Address("<REWARDS_RESOURCE_ADDRESS>")
+    Bucket("rewards")
+;
+CALL_METHOD
+    Address("<COMPONENT_ADDRESS>")
+    "deposit_future_rewards"
+    Bucket("rewards")
+;
+```
+
+### Assign rewards to users (an airdropper badge is needed)
+```
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "create_proof_of_amount"
+    Address("<AIRDROPPER_BADGE>")
+    Decimal("1")
+;
+CALL_METHOD
+    Address("<COMPONENT_ADDRESS>")
+    "assign_rewards"
+    Array<U64>(<USER_BADGE_ID>u64, <USER_BADGE_ID>u64...)
+    Array<Decimal>(Decimal("<AMOUNT>"), Decimal("<AMOUNT>")...)
+    Address("<REWARDS_RESOURCE_ADDRESS>")
+;
+```
+
+### Withdraw rewards (registered users only)
+```
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "create_proof_of_non_fungibles"
+    Address("<USER_BADGE>")
+    Array<NonFungibleLocalId>(NonFungibleLocalId("#<USER_BADGE_ID>#"))
+;
+POP_FROM_AUTH_ZONE
+    Proof("proof")
+;
+CALL_METHOD
+    Address("<COMPONENT_ADDRESS>")
+    "withdraw_rewards"
+    Proof("proof")
+;
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "deposit_batch"
+    Expression("ENTIRE_WORKTOP")
+;
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use scrypto::prelude::*;
 
-// Struct representing a 
+// Struct representing one of the coins to be used as rewards
 #[derive(ScryptoSbor)]
 struct Reward {
     vault: Vault,
@@ -8,6 +8,7 @@ struct Reward {
     assigned: KeyValueStore<u64, Decimal>,
 }
 
+// NonFungibleData of an user badge
 #[derive(Debug, ScryptoSbor, NonFungibleData)]
 struct UserNftData {
     id: u64,
@@ -16,6 +17,7 @@ struct UserNftData {
     last_rewards_withdraw: Instant,
 }
 
+// Event emitted at each user badge creation
 #[derive(ScryptoSbor, ScryptoEvent)]
 struct NewUserNftEvent {
     id: u64,
@@ -46,6 +48,7 @@ mod addix_fomo_rewards {
 
     impl AddixFomoRewards {
 
+        // Instantiate a new AddixFomoRewards component and globalize it
         pub fn new(
                 owner_badge_address: ResourceAddress,
                 airdropper_badge_address: ResourceAddress,
@@ -55,6 +58,7 @@ mod addix_fomo_rewards {
             let (address_reservation, component_address) =
                 Runtime::allocate_component_address(AddixFomoRewards::blueprint_id());
 
+            // Create a ResourceManager to mint user badges
             let user_nft_resource_manager = ResourceBuilder::new_integer_non_fungible_with_registered_type::<UserNftData>(
                 OwnerRole::Updatable(rule!(require(owner_badge_address)))
             )
@@ -88,6 +92,7 @@ mod addix_fomo_rewards {
             ))
             .create_with_no_initial_supply();
 
+            // Instantiate component
             Self {
                 user_nft_resource_manager: user_nft_resource_manager,
                 last_user_nft_id: 0,
@@ -102,15 +107,21 @@ mod addix_fomo_rewards {
             .globalize()
         }
 
+        // Mint a new user badge
         pub fn mint_user_nft(&mut self) -> Bucket {
+
+            // Increase the unique id
             self.last_user_nft_id += 1;
 
+            // Get the current time
             let now = Clock::current_time_rounded_to_seconds();
 
+            // Emit the NewUserNftEvent event to help bot development
             Runtime::emit_event(NewUserNftEvent {
                 id: self.last_user_nft_id,
             });
 
+            // Mint the NFT and return it
             self.user_nft_resource_manager.mint_non_fungible(
                 &NonFungibleLocalId::integer(self.last_user_nft_id.into()),
                 UserNftData {
@@ -121,10 +132,14 @@ mod addix_fomo_rewards {
             )
         }
 
+        // The OWNER can call this method to deposit future staking rewards for stakers
+        // The blueprint supports multiple different coins at the same time (make sure to not
+        // exceed 100 different coins or the transactions may fail)
         pub fn deposit_future_rewards(
             &mut self,
             future_rewards: Bucket
         ) {
+            // If there's already a Reward struct for this coin, just use it
             for i in 0 .. self.rewards.len() {
                 if self.rewards[i].vault.resource_address() == future_rewards.resource_address() {
                     self.rewards[i].vault.put(future_rewards);
@@ -132,6 +147,7 @@ mod addix_fomo_rewards {
                 }
             }
 
+            // If not, add a new Reward struct to the list
             self.rewards.push(
                 Reward {
                     vault: Vault::with_bucket(future_rewards),
@@ -141,6 +157,13 @@ mod addix_fomo_rewards {
             );
         }
 
+        // The airdropper bot can use this method to assing the previously deposited rewards to
+        // registered users.
+        // In case there are more than 100 registered stakers, you better call this method multiple
+        // times.
+        // The method checks that the total number of assigned coins for each resource do not
+        // exceed the deposited number of coins, you have to call the deposit_future_rewards method
+        // before this one.
         pub fn assign_rewards(
             &mut self,
             users: Vec<u64>,
@@ -152,11 +175,15 @@ mod addix_fomo_rewards {
                 "users and amounts have different lenght"
             );
 
+            // Search the coin in the list
             for j in 0 .. self.rewards.len() {
                 if coin == self.rewards[j].vault.resource_address() {
 
+                    // For each user id in the Vec
                     for i in 0 .. users.len() {
                         let user = users[i];
+
+                        // Check that the user exists
                         assert!(
                             user > 0 && user <= self.last_user_nft_id,
                             "User out of bounds: {}",
@@ -170,15 +197,20 @@ mod addix_fomo_rewards {
                             reward
                         );
 
+                        // Assign the reward to the user, eventually adding a new item to the
+                        // assigned KeyValueStore
                         if self.rewards[j].assigned.get(&user).is_some() {
                             *self.rewards[j].assigned.get_mut(&user).unwrap() += reward;
                         } else {
                             self.rewards[j].assigned.insert(user, reward);
                         }
 
+                        // Update the total number of assigned coins
                         self.rewards[j].total_assigned += reward;
                     }
 
+                    // Make sure the total of assigned coins is not greater than the total
+                    // deposited coins
                     assert!(self.rewards[j].vault.amount() >= self.rewards[j].total_assigned,
                         "assigned rewards > available rewards"
                     );
@@ -187,44 +219,60 @@ mod addix_fomo_rewards {
                 }
             }
 
+            // This coin has never beed deposited
             Runtime::panic("Coin not found".to_string());
         }
 
+        // Anyone with a user badge can use this method to withdraw his rewards.
         pub fn withdraw_rewards(
             &mut self,
             user_proof: Proof
         ) -> Vec<Bucket> {
+
+            // Check that the proof is valid
             let checked_proof = user_proof.check_with_message(
                 self.user_nft_resource_manager.address(),
                 "Incorrect proof",
             ).as_non_fungible();
 
+            // Get the NonFungibleData for the user badge
             let user_nft_data = checked_proof.non_fungible::<UserNftData>().data();
 
+            // Update the last_rewards_withdraw in the user badge
             self.user_nft_resource_manager.update_non_fungible_data(
                 &NonFungibleLocalId::Integer(user_nft_data.id.into()),
                 "last_rewards_withdraw",
                 Clock::current_time_rounded_to_seconds(),
             );
 
+            // Create a Vec of buckets to return to the user
             let mut buckets: Vec<Bucket> = vec![];
 
+            // For each coin
             for i in 0 .. self.rewards.len() {
                 let reward = &mut self.rewards[i];
+
+                // If the user has beed assigned this coin as reward
                 if reward.assigned.get(&user_nft_data.id).is_some() {
+
+                    // Take the rewards from the vault
                     let bucket = reward.vault.take_advanced(
                         *reward.assigned.get(&user_nft_data.id).unwrap(),
                         WithdrawStrategy::Rounded(RoundingMode::ToZero)
                     );
 
+                    // Update the user assigned rewards amount
                     *reward.assigned.get_mut(&user_nft_data.id).unwrap() -= bucket.amount();
 
+                    // Update the total assigned rewards number
                     reward.total_assigned -= bucket.amount();
 
+                    // Add the bucket to the list
                     buckets.push(bucket);
                 }
             }
 
+            // Return all of the reward buckets to the user
             buckets
         }
     }
